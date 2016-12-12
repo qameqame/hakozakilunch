d85a3666e02d2cdee2d4415e17026bdfca2d2507
hkameyam <hkameyam@gmail.com>
2016-12-12 18:59:02 +0900

    chenge the first message.

--- web/callback.php
+++ web/callback.php
@@ -134,10 +134,10 @@ if ($text == 'はい') {
 } else {
   $response_format_text = [
     "type" => "template",
-    "altText" => "こんにちわ 何かご用ですか？（はい／いいえ）",
+    "altText" => "そろそろランチタイムですか？（はい／いいえ）",
     "template" => [
         "type" => "confirm",
-        "text" => "こんにちわ 何かご用ですか？",
+        "text" => "そろそろランチタイムですか？",
         "actions" => [
             [
               "type" => "message",
